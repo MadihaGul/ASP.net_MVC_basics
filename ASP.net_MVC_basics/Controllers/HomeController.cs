452b677e92d16d479f80d074b44ebe264a6caf9f
madiha gul <mmadihagull@gmail.com>
2021-11-16 15:48:01 +0100

    Add project files.

---
+++ ASP.net_MVC_basics/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP.net_MVC_basics.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewBag.Message = "";
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+          
+            return View();
+        }
+
+        public IActionResult Projects()
+        {
+            ViewBag.Message = "";
+            return View();
+        }
+    }
+}
